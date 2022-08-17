2ca19d5d802ab13accc18ba109ec6dad06dfc93f
lll555@yandex.ru <lll555@yandex.ru>
2022-08-17 08:45:57 +0000

    Edited files:     /src/scripts/check.js

--- src/scripts/check.js
+++ src/scripts/check.js
@@ -2,13 +2,14 @@ function checkNumber(userGuess) {
     //var userGuess = $parseTree._UserGuess;
             
     //$reactions.answer(checkNumber('a'));
-            
-            
+    if (String(userGuess).length != 4) {
+        return 'Нужно написать четыре цифры';
+    }        
     if (userGuess == $jsapi.context().session.secretNumber) {
         return 'Угадал!';
     //    $jsapi.context().reactions.answer('Угадал!');
-    } else {
-        return 'Не угадал.';
-    //    $jsapi.context().reactions.answer('НЕ угадал!');
-    }
+    } 
+
+    return 'Не угадал.';
+
 }
