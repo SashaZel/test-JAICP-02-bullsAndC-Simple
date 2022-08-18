2f3e00a8226a774cadc892d7086d091472134efd
lll555@yandex.ru <lll555@yandex.ru>
2022-08-18 03:33:26 +0000

    Edited files:     /src/scripts/numberGenerator.js

--- src/scripts/numberGenerator.js
+++ src/scripts/numberGenerator.js
@@ -1,7 +1,15 @@
 function createNewSecretNumber() {
-    
-    $jsapi.context().session.numberOfAttempts = 0;
+    //var resultNumber = [0, 0, 0, 0];
+    //for (var i = 0; i < 4 ; i++) {
+    //    resultNumber = String(Math.floor(Math.random() * 10));    
+    //}
+    //$jsapi.context().session.numberOfAttempts = 0;
     // Set secret number
-    $jsapi.context().session.secretNumber = ['1', '2', '3', '4'];
-    return ['1', '2', '3', '4']; 
+    //$jsapi.context().session.secretNumber = ['1', '2', '3', '4'];
+    //return ['1', '2', '3', '4']; 
+    return ['', '', '', ''].map(
+        function () {
+            return String(Math.floor(Math.random() * 10)); 
+        }    
+    );
 }
