abfbe13dd28483b2732dffb3ce5907cf758f513c
lll555@yandex.ru <lll555@yandex.ru>
2022-08-18 07:19:12 +0000

    Edited files:     /src/scripts/check.js

--- src/scripts/check.js
+++ src/scripts/check.js
@@ -2,8 +2,8 @@ function checkNumber(userGuess, secretNumber) {
     if (!userGuess || !secretNumber) {
         return 'Ой, у меня ошибка. Игра не началась. Напиши "Да", чтобы поигать, или "Правила", чтобы прочитать правила.'
     }
-    if (String(userGuess).length !== 4) {
-        return 'Нужно написать четыре цифры';
+    if (!inputISvalid(userGuess)) {
+        return 'Ошибка ввода. Нужно написать четыре цифры';
     }         
     // Increment number of attempts
     $jsapi.context().session.numberOfAttempts += 1;
