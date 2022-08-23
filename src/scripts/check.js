49acd1652ca12b7085136e2acb030dcb3a8735aa
lll555@yandex.ru <lll555@yandex.ru>
2022-08-23 01:45:37 +0000

    Edited files:     /src/scripts/check.js

--- src/scripts/check.js
+++ src/scripts/check.js
@@ -19,6 +19,18 @@ function checkNumber(userGuess, secretNumber) {
     //         return '';
     //     }
     // ).join(' ');
+    var bulls = 0;
+    var cows = 0;
+    
+    for ( var i = 0; i < 4; i++ ) {
+        if (userGuess[i] === secretNumber[i]) {
+            bulls += 1;
+        } 
+        if (secretNumber.indexOf(userGuess[i]) >= 0 && userGuess[i] === secretNumber[i]) {
+            cows += 1;
+        }
+    }
+    
     return [ 3, 5 ];
 
 }
