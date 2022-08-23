47c80f46661924963daadbcb777effe4d2ea9b11
lll555@yandex.ru <lll555@yandex.ru>
2022-08-23 04:55:01 +0000

    Edited files:     /src/scripts/numberGenerator.js

--- src/scripts/numberGenerator.js
+++ src/scripts/numberGenerator.js
@@ -1,10 +1,22 @@
-function createNewSecretNumber() {
-    // pure funct
-    // Hardcoded test dummy data below
-    // return ['1', '2', '3', '4']; 
+function createValidSecretNumber () {
     return ['', '', '', ''].map(
         function () {
             return String($reactions.random(10)); 
         }    
     );
 }
+
+// validation of secret number
+// in fact this feature is redundant cause docs specified $reactions.random(max) as a method
+// with check of not repeating returned random value more often than once per every max/2 
+
+function createNewSecretNumber() {
+    var resultNumber;
+    
+    while (!inputIsValid(resultNumber.join(''))) {
+        resultNumber = createValidSecretNumber();
+    }
+    
+    return resultNumber;
+}
+
